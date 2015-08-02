fa4b8271546d85072b27de185e30277482ae6ac7
Miguel Pineda <miguel25mayo@gmail.com>
2015-08-02 13:44:07 +0200

    Varias Preguntas y Autoload

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -1,20 +1,35 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function (req, res){
-  models.Quiz.findAll().then( function(quiz){
-    res.render('quizes/question', {pregunta: quiz[0].pregunta});
-  });
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      }else{
+        next(new Error('No existe quizId=' + quizId));
+      }
+    }
+  ).catch(function(error){next(error)});
+};
+
+exports.index = function (req, res){
+  models.Quiz.findAll().then(function(quizes){
+    res.render('quizes/index', {quizes: quizes});
+  }).catch(function(error){next(error);})
+} ;
+
+// GET /quizes/:id
+exports.show = function (req, res){
+    res.render('quizes/show', {quiz: req.quiz});
 
 };
 
 exports.answer = function (req , res){
-  models.Quiz.findAll().then(function(quiz){
-    if (req.query.respuesta === quiz[0].respuesta){
-      res.render('quizes/answer', {respuesta: 'Correcto'});
-    }else{
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+  var resultado = "Incorrecto";
+    if (req.query.respuesta === req.quiz.respuesta){
+      resultado = "Correcto";
     }
-  });
+    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 
 };
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz - MPL' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.param('quizId', quizController.load);
+
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res) {
   res.render('author' );
