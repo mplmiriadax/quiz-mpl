afbba4f037636bc7d0bbc941d88040c74971023a
Miguel Pineda <miguel25mayo@gmail.com>
2015-08-03 23:29:17 +0200

    Con tematica

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -49,7 +49,7 @@ exports.create = function(req, res){
       if (err){
         res.render('quizes/new', {quiz:quiz, errors:err.errors});
       }else{
-        quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+        quiz.save({fields: ["pregunta","respuesta","tema"]}).then(function(){
           res.redirect('/quizes');
         })
       }
@@ -67,12 +67,13 @@ exports.update = function(req, res) {
 
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz.validate().then(function(err){
     if (err) {
       res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
     } else {
-      req.quiz.save( {fields: ["pregunta", "respuesta"]})
+      req.quiz.save( {fields: ["pregunta", "respuesta","tema"]})
       .then( function(){ res.redirect('/quizes');});
     }
   }
